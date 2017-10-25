ca11b7efc76613f080dee8f806b2c58806e8e8c8
Timen Olthof <timen@timenolthof.nl>
2017-10-25 13:13:18 +0200

    feat: add MyAccount page and require email attribute to view

---
+++ src/containers/MyAccount/MyAccount.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+class MyAccount extends Component {
+  render() {
+    const { emails } = this.props;
+
+    return (
+      <div>
+        <h2>My Account</h2>
+
+        <br/><br/>
+        Email Address: { (emails && emails.length > 0) ? emails[0] : "No email address available."}
+        <br/>
+      </div>
+    );
+  }
+}
+
+MyAccount.propTypes = {
+  emails: PropTypes.array.isRequired,
+}
+
+function mapStateToProps(state) {
+  const { user } = state
+  return {
+    emails: user.attributes['pbdf.pbdf.email.email'],
+  }
+}
+
+export default connect(mapStateToProps)(MyAccount);
