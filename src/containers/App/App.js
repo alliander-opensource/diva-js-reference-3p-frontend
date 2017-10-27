fe832de2e46bbc4c21fafd7ceca03c4729db01a8
Timen Olthof <timen@timenolthof.nl>
2017-10-27 10:54:53 +0200

    feat: add deauthentication function

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { connect } from 'react-redux';
 import AppBar from 'material-ui/AppBar';
 import Paper from 'material-ui/Paper';
+import IconMenu from 'material-ui/IconMenu';
 import IconButton from 'material-ui/IconButton';
 import IconSocialPerson from 'material-ui/svg-icons/social/person';
+import MenuItem from 'material-ui/MenuItem';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
 import WithDivaAuthorization from '../../util/WithDivaAuthorization';
@@ -15,6 +18,8 @@ import  MyAccount from '../../containers/MyAccount/MyAccount';
 import  MyHome from '../../containers/MyHome/MyHome';
 import  UserInfo from '../../containers/UserInfo/UserInfo';
 
+import { deauthenticate } from '../../actions';
+
 import './App.css';
 
 const styles = {
@@ -25,49 +30,41 @@ const styles = {
 };
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      showMenu: true,
-      showUserInfo: true,
-    };
-  }
-
-  toggleMenu() {
-    this.setState(previousState => {
-      return {
-        showMenu: !previousState.showMenu
-      };
-    });
-  }
-
-  toggleUserInfo() {
-    this.setState(previousState => {
-      return {
-        showUserInfo: !previousState.showUserInfo
-      };
-    });
+  handleDeauth() {
+    const { dispatch } = this.props;
+    dispatch(deauthenticate());
   }
 
   render() {
+    const RightMenu = (props) => (
+      <IconMenu
+        {...props}
+        iconButtonElement={
+          <IconButton><IconSocialPerson style={{color: 'red'}}/></IconButton>
+        }
+        targetOrigin={{horizontal: 'right', vertical: 'top'}}
+        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+      >
+        <Link to='/'>
+          <MenuItem primaryText="About" />
+        </Link>
+        <MenuItem primaryText="Clear session" onClick={ () => this.handleDeauth() }
+/>
+      </IconMenu>
+    );
+
     return (
       <BrowserRouter>
         <div>
           <AppBar
             title="DIVA 3rd party reference implementation"
-            iconElementRight={<IconButton><IconSocialPerson/></IconButton>}
-            onLeftIconButtonTouchTap={ () => this.toggleMenu() }
-            onRightIconButtonTouchTap={ () => this.toggleUserInfo() }
+            iconElementRight={<RightMenu/>}
           />
           <Grid fluid>
             <Row>
-              {
-                this.state.showMenu &&
-                <Col xs={12} sm={3}>
-                  <SideMenu/>
-                </Col>
-              }
+              <Col xs={12} sm={3}>
+                <SideMenu/>
+              </Col>
 
               <Col xs>
                 <Paper style={styles.main}>
@@ -77,12 +74,9 @@ class App extends Component {
                 </Paper>
               </Col>
 
-              {
-                this.state.showUserInfo &&
-                <Col xs={12} sm={3}>
-                  <UserInfo/>
-                </Col>
-              }
+              <Col xs={12} sm={3}>
+                <UserInfo/>
+              </Col>
             </Row>
           </Grid>
         </div>
@@ -91,4 +85,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
