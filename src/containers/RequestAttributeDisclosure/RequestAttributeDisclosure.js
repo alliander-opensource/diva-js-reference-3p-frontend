6153587f69576f6ca9b41b433c301d6ee7f6a8b8
Timen Olthof <timen@timenolthof.nl>
2017-10-30 15:41:44 +0100

    feat: add screens for edge cases during disclosure

--- src/containers/RequestAttributeDisclosure/RequestAttributeDisclosure.js
+++ src/containers/RequestAttributeDisclosure/RequestAttributeDisclosure.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import QRCode from 'qrcode.react';
 import { Row, Col } from 'react-flexbox-grid';
+import RaisedButton from 'material-ui/RaisedButton';
 import CircularProgress from 'material-ui/CircularProgress';
 import IconActionCheckCircle from 'material-ui/svg-icons/action/check-circle';
 import IconActionHelp from 'material-ui/svg-icons/action/help';
@@ -19,29 +20,39 @@ class RequestAttributeDisclosure extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      disclosureStatus: 'PENDING',
+      serverStatus: 'INITIALIZED',
       sessionStarted: false,
     };
   }
 
   componentDidMount() {
     this._isMounted = true;
-    const { requiredAttribute } = this.props;
-
     if (!this.state.sessionStarted) {
-      fetch(`/api/start-disclosure-session?attribute=${requiredAttribute}&attributesLabel=${requiredAttribute}`, {
-        credentials: 'include'
-      })
-      .then(response => response.json())
-      .then(data => {
-        if (this._isMounted) {
-          this.setState({
-            qrContent: data.qrContent,
-          });
-          this.startPolling(data.irmaSessionId);
-          setTimeout(this.stopPolling, 30000);
-        }
-      });
+      this.fetchQR();
     }
+  }
+
+  fetchQR = () => {
+    const { requiredAttribute } = this.props;
+    this.setState({
+      disclosureStatus: 'PENDING',
+      serverStatus: 'INITIALIZED',
+      sessionStarted: true,
+    });
+    fetch(`/api/start-disclosure-session?attribute=${requiredAttribute}&attributesLabel=${requiredAttribute}`, {
+      credentials: 'include'
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (this._isMounted) {
+        this.setState({
+          qrContent: data.qrContent,
+        });
+        this.startPolling(data.irmaSessionId);
+        setTimeout(this.stopPolling, 30000);
+      }
+    });
   }
 
   startPolling = irmaSessionId => {
@@ -63,9 +74,11 @@ class RequestAttributeDisclosure extends Component {
         console.log(result);
         self.setState({
           disclosureStatus: result.disclosureStatus,
+          serverStatus: result.serverStatus,
           proofStatus: result.proofStatus,
         });
-        if (result.disclosureStatus === 'COMPLETED') {
+        if (result.disclosureStatus === 'COMPLETED' ||
+            result.disclosureStatus === 'ABORTED') {
           self.stopPolling();
           setTimeout(() => { self.refreshSession() }, 2000);
         }
@@ -89,18 +102,26 @@ class RequestAttributeDisclosure extends Component {
   }
 
   render() {
+    console.log('STATE', this.state);
     const { requiredAttribute } = this.props;
-    const { qrContent, disclosureStatus, proofStatus } = this.state;
+    const {
+      qrContent,
+      disclosureStatus,
+      proofStatus,
+      serverStatus
+    } = this.state;
 
     return (
       <div>
         {qrContent ? (
           <div>
-            {(disclosureStatus !== "COMPLETED") ? (
+
+            {(disclosureStatus === 'PENDING') && (
               <div>
+
                 <Toolbar style={{ backgroundColor: 'none' }}>
                   <ToolbarGroup>
-                    <ToolbarTitle text="Attribute Required" />
+                    <ToolbarTitle text='Attribute Required' />
                   </ToolbarGroup>
                   <ToolbarGroup lastChild={true}>
                     <IconButton tooltip="Help">
@@ -111,30 +132,47 @@ class RequestAttributeDisclosure extends Component {
                     </IconButton>
                   </ToolbarGroup>
                 </Toolbar>
-                <div style={{ padding: '20px' }}>
-                  <Row center="xs">
-                    <Col xs={6}>
-                      In order to view this page, the <b>{requiredAttribute}</b> attribute is required.<br/>
-                      <br/>
-                    </Col>
-                  </Row>
-                  <Row center="xs">
-                    <Col xs>
-                      <QRCode value={JSON.stringify(qrContent)} size={256}/><br/>
-                      <br/>
-                    </Col>
-                  </Row>
-                  <Row center="xs">
-                    <Col xs={6}>
-                      Please scan the QR code with your IRMA app to continue.
-                      <br/>
-                    </Col>
-                  </Row>
-                </div>
+
+                {(serverStatus === 'INITIALIZED') && (
+                  <div style={{ padding: '20px' }}>
+                    <Row center="xs">
+                      <Col xs={6}>
+                        In order to view this page, the <b>{requiredAttribute}</b> attribute is required.<br/>
+                        <br/>
+                      </Col>
+                    </Row>
+                    <Row center="xs">
+                      <Col xs>
+                        <QRCode value={JSON.stringify(qrContent)} size={256}/><br/>
+                        <br/>
+                      </Col>
+                    </Row>
+                    <Row center="xs">
+                      <Col xs={6}>
+                        Please scan the QR code with your IRMA app to continue.
+                        <br/>
+                      </Col>
+                    </Row>
+                  </div>
+                )}
+
+                {(serverStatus === 'CONNECTED') && (
+                  <div style={{ padding: '20px' }}>
+                    <Row center="xs">
+                      <Col xs={6}>
+                        To continue, approve attribute disclosure with your IRMA app.<br/>
+                        <br/>
+                      </Col>
+                    </Row>
+                  </div>
+                )}
+
               </div>
-            ) : (
+            )}
+
+            {(disclosureStatus === 'COMPLETED') && (
               <div>
-                {(proofStatus === "VALID") ? (
+                {(proofStatus === 'VALID') ? (
                   <div>
                     <Row center="xs">
                       <Col xs>
@@ -156,7 +194,58 @@ class RequestAttributeDisclosure extends Component {
                     </Row>
                     <Row center="xs">
                       <Col xs={6}>
-                        Oops, something went wrong!
+                        Oops, something went wrong!<br/>
+                        <br/>
+                        <RaisedButton label="Retry"
+                          primary={true} style={{}}
+                          onClick={() => this.fetchQR()}/>
+                      </Col>
+                    </Row>
+                  </div>
+                )}
+              </div>
+            )}
+            {(disclosureStatus === 'ABORTED') && (
+              <div>
+              <Toolbar style={{ backgroundColor: 'none' }}>
+                <ToolbarGroup>
+                  <ToolbarTitle text="Disclosure cancelled" />
+                </ToolbarGroup>
+                <ToolbarGroup lastChild={true}>
+                  <IconButton tooltip="Help">
+                    <IconActionHelp/>
+                  </IconButton>
+                  <IconButton tooltip="Info">
+                    <IconActionInfo/>
+                  </IconButton>
+                </ToolbarGroup>
+              </Toolbar>
+
+                {(serverStatus === 'CANCELLED') && (
+                  <div style={{ padding: '20px' }}>
+                    <Row center="xs">
+                      <Col xs={6}>
+                        You cancelled attribute disclosure.<br/>
+                        <br/>
+                        <RaisedButton label="Retry"
+                          primary={true} style={{}}
+                          onClick={() => this.fetchQR()}/>
+                        <br/>
+                      </Col>
+                    </Row>
+                  </div>
+                )}
+
+                {(serverStatus === 'NOT_FOUND') && (
+                  <div style={{ padding: '20px' }}>
+                    <Row center="xs">
+                      <Col xs={6}>
+                        The QR code expired.<br/>
+                        <br/>
+                        <RaisedButton label="Retry"
+                          primary={true} style={{}}
+                          onClick={() => this.fetchQR()}/>
+                        <br/>
                       </Col>
                     </Row>
                   </div>
