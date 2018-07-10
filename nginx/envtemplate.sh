99b0e7fd8c41bb589feb98e6cfc12b17743c22e9
Koen van Ingen <koenvaningen@gmail.com>
2018-07-10 11:17:37 +0200

    chore: simplified loading of BASE_URL

--- nginx/envtemplate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ -n "$1" ] && [ -n "$2" ]
-then
-  echo "Adding env vars to template $1"
-  perl -pe 's;(\\*)(\$([a-zA-Z_][a-zA-Z_0-9]*)|\$\{([a-zA-Z_][a-zA-Z_0-9]*)\})?;substr($1,0,int(length($1)/2)).($2&&length($1)%2?$2:$ENV{$3||$4});eg' $1 > $2
-else
-  echo "Usage: envtemplate <template> <destination>"
-fi
