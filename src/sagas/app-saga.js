388bb776c75971adb7438876ea1130484838ae0c
Timen Olthof <timen@timenolthof.nl>
2018-09-19 11:40:55 +0200

    fix: bugs (timeouts, robustness, wrong credentialType due to spelling error)

--- src/sagas/app-saga.js
+++ src/sagas/app-saga.js
@@ -5,15 +5,14 @@ import { types as divaTypes, actions } from 'diva-react';
 function* onPollResult(action) {
   if (action.data.serverStatus === 'CONNECTED') {
     yield put(actions.abandonIrmaSession(action.viewId, action.irmaSessionId));
-    yield put(actions.startIrmaSession(action.viewId, 'ISSUE', { credentialsType: 'FIELDLAB' }));
+    yield put(actions.startIrmaSession(action.viewId, 'ISSUE', { credentialType: 'FIELDLAB' }));
   }
 }
 
 function* onSessionCompleted(action) {
-  if (action.data.serverStatus === 'NOT_FOUND') {
-    yield put(actions.abandonIrmaSession(action.viewId, action.irmaSessionId));
-    yield put(actions.startIrmaSession(action.viewId, 'ISSUE', { credentialsType: 'FIELDLAB' }));
-  }
+  // For robustness, always start a new session
+  yield put(actions.abandonIrmaSession(action.viewId, action.irmaSessionId));
+  yield put(actions.startIrmaSession(action.viewId, 'ISSUE', { credentialType: 'FIELDLAB' }));
 }
 
 function* sagas() {
