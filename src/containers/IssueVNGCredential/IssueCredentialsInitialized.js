3e3d92a4352498541dfafc7fb14347dacb227466
Sietse Ringers <mail@sietseringers.net>
2018-09-28 10:46:09 +0200

    Remove explaining text

--- src/containers/IssueVNGCredential/IssueCredentialsInitialized.js
+++ src/containers/IssueVNGCredential/IssueCredentialsInitialized.js
@@ -6,21 +6,9 @@ import { Row, Col } from 'react-flexbox-grid';
 const IssueCredentialsInitialized = ({ qrContent }) => (
   <div style={{ padding: '20px' }}>
     <Row center="xs">
-      <Col xs={6}>
-        <h2>Verkrijg hier uw Fieldlabdeelnemer-attribuut!</h2><br />
-        <br />
-      </Col>
-    </Row>
-    <Row center="xs">
       <Col xs>
-        <QRCode value={JSON.stringify(qrContent)} size={256} /><br />
+        <QRCode value={JSON.stringify(qrContent)} size={384} /><br />
         <span style={{ display: 'none' }} id="qr-content">{JSON.stringify(qrContent)}</span>
-        <br />
-      </Col>
-    </Row>
-    <Row center="xs">
-      <Col xs={6}>
-        Scan de code met uw IRMA app om het VNG fieldlab-deelnemer credential te ontvangen.
         <br />
       </Col>
     </Row>
