d5ed854f8478d70e24fdba5948d84873d057b285
Timen Olthof <timen@timenolthof.nl>
2018-10-25 10:16:57 +0200

    feat: added menu items for vrn and bsn demo credentials

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -8,6 +8,8 @@ import IconSocialPerson from 'material-ui/svg-icons/social/person';
 import IconAssignmentReturned from 'material-ui/svg-icons/action/assignment-returned';
 import IconAddLocation from 'material-ui/svg-icons/maps/add-location';
 import IconCreate from 'material-ui/svg-icons/content/create';
+import IconDirectionsCar from 'material-ui/svg-icons/maps/directions-car';
+import IconPermIdentity from 'material-ui/svg-icons/action/perm-identity';
 
 const SideMenu = () => {
   const style = {
@@ -41,6 +43,12 @@ const SideMenu = () => {
           </Link>
           <Link to="/issue-ean">
             <ListItem primaryText="Issue EAN" leftIcon={<IconAssignmentReturned />} />
+          </Link>
+          <Link to="/issue-vrn">
+            <ListItem primaryText="Issue VRN" leftIcon={<IconDirectionsCar />} />
+          </Link>
+          <Link to="/issue-bsn">
+            <ListItem primaryText="Issue BSN" leftIcon={<IconPermIdentity />} />
           </Link>
         </List>
       </Paper>
