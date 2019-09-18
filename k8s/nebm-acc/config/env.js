d9b42cc683a2a65921629b96cd7ef216aa3875de
Koen van Ingen <koenvaningen@gmail.com>
2019-09-18 18:36:17 +0200

    chore: updated k8s acc and prd config

--- k8s/nebm-acc/config/env.js
+++ k8s/nebm-acc/config/env.js
@@ -1,3 +1,14 @@
 window.env = {
   baseUrl: 'https://acc-diva-js-reference-3p.appx.cloud',
+  irmaUrl: 'https://dev-diva-irma-server.appx.cloud',
+  jwtEnabled: true,
+  jwtPublicKey: `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu96px180vnxpN8fJskhj
+yKzOIucjxqeQ6ks6Nr0uO87NCo/TL7nf+eoK4a+w/bCNEPgcrOyrvzyHtBacPeZK
+4RJcJ25OhJ9IUfdo+a7NeDuuv0ofuWO2Vvvw8tF9udTlk5+IMh02WbdVX87ULkax
+7LDLI/AlBX8gKgK3N8bpTxPi9LnMpC4hq7CqzEk02UZ+d/J3TBdISoaCOTyXtzR2
+eQOhfOWKBXTpKWtP7DgcxYwEklxcKUPfIEeUb60KBVQnXJDhlOSI51RltAunrSfR
+eX2csN9IJI88JieTDVTR9rah7Y/ils6Mu9W8fcgL1n278YGBnByR8fBISzSN8PRt
++wIDAQAB
+-----END PUBLIC KEY-----`,
 };
